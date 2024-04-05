05ac0abf437d17c4d1528e68c73efa8831292523
balaji3123 <balajidhandapani00@gmail.com>
2024-04-06 01:57:37 +0530

    finished user card of dass and co at 6-4-24 night 2 am

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,25 +1,57 @@
-    function User(){
+   
+   import PropTypes from "prop-types";
+   const userData =[
+        {
+        name: "harold",
+        city: "telungana",
+        jobrole: "datura factory lead",
+        skills:["killing","trading","fighting","planting","smoking"],
+        online:true,
+        profile:"profilepic5.jpg"
+        },
+        {   name: "leo",
+            city: "kashmir",
+            jobrole: "datura factory ex sub lead",
+            skills:["killing","trading","fighting","planting","smoking"],
+            online:false,
+            profile:"profilepic4.jpg"
+
+        },
+        
+        {   name: "elisa",
+            city: "telungana",
+            jobrole: "datura factory ex sub lead",
+            skills:["drinking", "dying", "fighting", "crying","dancing"],
+            online:false,
+            profile:"profilepic6.jpg"
+
+        }
+
+
+    ]
+
+    
+    function User(props){
         return(
             <div className="cardContainer">
-                <span className="pro">online</span>
-                <img className="img" src="profilepic1.jpg" alt="" />
-                <h3>Full name</h3>
-                <h3>sydney</h3>
-                <p>front end developer</p>
+                <span className={props.online ? "pro online": "pro offline"}>{props.online ? "ONLINE" : "OFFLINE"}</span>
+                <img className="img" src={props.profile} alt="" />
+                <h3>{props.name}</h3>
+                <h3>{props.city}</h3>
+                <p>{props.jobrole}</p>
                 <div className="buttons">
                     <button className="b1">message</button>
-                    <button className="b2">follow</button>
+                    <button className="b1  b2">following</button>
                 </div>
 
                 <div className="skills">
-                    <h6>skills</h6>
+                    <h6>SKILLS</h6>
                     <ul>
-                        <li>react</li>
-                        <li>html</li>
-                        <li>css</li>
-                        <li>bootstrap</li>
-                        <li>ui/ux</li>
-                        <li>mysql</li>
+                        {props.skills.map((skill, index)=>(
+                            <li key={index}>{skill}</li>
+                        ))}
+                        
+                       
 
                     </ul>
                 </div>
@@ -33,9 +65,27 @@
     
     export const UserCard = () => {
       return (
-        <div>
-            <User />
-        </div>
+        <>{userData.map((user,index)=>(
+            <User key={index}
+            name={user.name}
+            city={user.city}
+            jobrole={user.jobrole}
+            online={user.online}
+            profile={user.profile}
+            skills={user.skills} />
+        ))}
+        </>
       )
+    }
+
+    // <User  name="james" city="new york" jobrole="react developer" skills={["html","css","js","bootstrap","vite"]} online ={true} profile="profilepic2.jpg" />
+
+    User.propTypes = {
+        name:PropTypes.string.isRequired,
+        city:PropTypes.string.isRequired,
+        jobrole:PropTypes.string.isRequired,
+        skills:PropTypes.arrayOf(PropTypes.string).isRequired,
+        online:PropTypes.bool.isRequired,
+        profile: PropTypes.string.isRequired
     }
     
